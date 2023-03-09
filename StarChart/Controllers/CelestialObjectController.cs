6c8cea8a6b65302eebf0c16ee4018b038c5d82a3
Mohan yaswanth kumar reddy <YawanthReddy6166@users.noreply.github.com>
2023-03-09 20:47:00 +0530

    created CelestialObjectController contructor

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -8,7 +8,7 @@ namespace StarChart.Controllers
     public class CelestialObjectController:ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        CelestialObjectController(ApplicationDbContext context)
+       public  CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
         }
